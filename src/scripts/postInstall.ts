c9623a8cd9f5b3188d032937f320383a8b83000d
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-18 09:31:30 +0100

    fix: 🐛 Delete scripts

--- src/scripts/postInstall.ts
+++
@@ -1,8 +0,0 @@
-import Configstore from 'configstore'
-
-try {
-    new Configstore('solve3-cli', { username: '', password: '', authCookie: '', favorites: [] })
-    console.log('Config file has been created!')
-} catch (err) {
-    console.error(err)
-}
--- src/scripts/preUninstall.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-import Configstore from 'configstore'
-
-const config = new Configstore('solve3-cli')
-fs.unlink(config.path, (err) => {
-    if (err) throw err
-    console.log('Config file has been deleted!')
-})
