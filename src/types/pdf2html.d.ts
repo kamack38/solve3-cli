7511172de88e75c1028265b583dd75b2ffd84cfe
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-06-07 14:54:50 +0200

    refactor: 💡 Update pdf2html types

--- src/types/pdf2html.d.ts
+++ src/types/pdf2html.d.ts
@@ -1,12 +1,13 @@
 declare module 'pdf2html' {
     import type { PathLike } from 'node:fs'
-    export const html: (filePath: PathLike, callback: (err: Error, res: string) => void) => void
-    export const text: (filePath: PathLike, callback: (err: Error, res: string) => void) => void
-    export const pages: (filePath: PathLike, callback: (err: Error, res: string) => void) => void
-    export const meta: (filePath: PathLike, callback: (err: Error, res: string) => void) => void
+    type callback = (err: Error, res: string) => void
+    export const html: (filePath: PathLike, callback: callback) => void
+    export const text: (filePath: PathLike, callback: callback) => void
+    export const pages: ((filePath: PathLike, callback: callback) => void) | ((filePath: PathLike, options: { text: boolean }, callback: callback) => void)
+    export const meta: (filePath: PathLike, callback: callback) => void
     export const thumbnail: (
         filePath: PathLike,
-        options: { page: number; imageType: 'png' | 'jpg'; width: number; height: number } | ((err: Error, res: string) => void),
+        options: { page: number; imageType: 'png' | 'jpg'; width: number; height: number } | callback,
         callback?: (err: Error, res: string) => void,
     ) => void
 }
